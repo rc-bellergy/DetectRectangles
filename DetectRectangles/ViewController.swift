e764b8193033c6922c249e45b09ffbc00b1f971a
Michael <michael@designquest.com.hk>
2019-10-10 18:04:13 +0800

    stable

--- DetectRectangles/ViewController.swift
+++ DetectRectangles/ViewController.swift
@@ -16,17 +16,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     var session = AVCaptureSession()
     var requests = [VNRequest]()
+    var capteredBuffer:CVImageBuffer? // current captured video buffer
     
     // MARK: - Config
     
     var sessionPreset = AVCaptureSession.Preset.high // change the capture quality here
-    var maximumObservations = 0 // Allows Vision algorithms to return the number of observations.
+    var maximumObservations = 1 // Allows Vision algorithms to return the number of observations.
     var minimumSize:Float = 0.1 // the minimum size of the rectangle to be detected (0 - 1)
     
     // MARK - Outlets
     
     @IBOutlet weak var videoImageView: UIImageView!
     @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var previewImageView: UIImageView!
     
     // MARK: - Override
     
@@ -36,23 +38,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         startDetection()
     }
     
-    // MARK: - Delegate
-    
-    // AVCaptureVideoDataOutputSampleBufferDelegate
-    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        var requestOptions:[VNImageOption : Any] = [:]
-        if let camData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
-            requestOptions = [.cameraIntrinsics:camData]
-        }
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation.right, options: requestOptions)
-        do {
-            try imageRequestHandler.perform(self.requests) // pass the analysis requests here
-        } catch {
-            print(error)
-        }
-    }
-    
     // MARK: - Video session
     
     private func startLiveVideo() {
@@ -60,14 +45,41 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             session.sessionPreset = self.sessionPreset
             let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
             let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(deviceInput)
+
             let deviceOutput = AVCaptureVideoDataOutput()
             deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
-            session.addInput(deviceInput)
+            deviceOutput.videoSettings = [
+                String(kCVPixelBufferPixelFormatTypeKey):
+                NSNumber(value: kCVPixelFormatType_32BGRA)
+            ]
             session.addOutput(deviceOutput)
+            
             let videoLayer = AVCaptureVideoPreviewLayer(session: session)
             videoLayer.frame = videoImageView.bounds
             videoImageView.layer.addSublayer(videoLayer)
+            
             session.startRunning()
+        }
+    }
+    
+    // MARK: - Delegate
+    
+    // AVCaptureVideoDataOutputSampleBufferDelegate
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        self.capteredBuffer = pixelBuffer
+
+        var requestOptions:[VNImageOption : Any] = [:]
+        if let camData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics:camData]
+        }
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation.right, options: requestOptions)
+        
+        do {
+            try imageRequestHandler.perform(self.requests) // pass the analysis requests here
+        } catch {
+            print(error)
         }
     }
     
@@ -83,6 +95,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     private func detectionHandler(request: VNRequest, error: Error?) {
         guard let results = request.results else { return }
         let observations = results.map({$0 as! VNRectangleObservation})
+
         DispatchQueue.main.async() {
             let frame = self.infoView.bounds
             
@@ -111,4 +124,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         return rec
     }
 
+}
+
+extension CGPoint {
+   func scaled(to size: CGSize) -> CGPoint {
+       return CGPoint(x: self.x * size.width,
+                      y: self.y * size.height)
+   }
 }
